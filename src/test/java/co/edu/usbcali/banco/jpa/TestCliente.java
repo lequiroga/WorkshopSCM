f564a6c0d7471b95e3cfa544fdd72aba92181df1
kike <kike@CALI-1912>
2018-04-21 16:02:16 -0500

    seventh commit

--- src/test/java/co/edu/usbcali/banco/jpa/TestCliente.java
+++ src/test/java/co/edu/usbcali/banco/jpa/TestCliente.java
@@ -93,7 +93,7 @@ class TestCliente {
 	@Test
 	@DisplayName("ConsultarClientePorId")
 	void btest() {
-		assertNotNull(entityManager, "El entitymanager es nulo");
+		assertNotNull(entityManager, "El entitymanager is null");
 		Cliente cliente=entityManager.find(Cliente.class, clieId);
 		assertNotNull(cliente, "El cliente ya existe");
 		
